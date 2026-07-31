a4a1a0d69686765264f5b9f8c92b9fd2c375a9c2
Evan Binu Chirayath <143860161+EvanBinu@users.noreply.github.com>
2026-07-31 18:35:57 +0530

    Time: 184 ms (25%), Space: 19.6 MB (35.37%) - LeetHub

--- 2024-maximize-the-confusion-of-an-exam/2024-maximize-the-confusion-of-an-exam.py
+++ 2024-maximize-the-confusion-of-an-exam/2024-maximize-the-confusion-of-an-exam.py
@@ -4,12 +4,9 @@ class Solution:
         maxfreq=maxlen=left= 0
         n = len(answerKey)
         for right in range(n):
-            if answerKey[right] in map:
-                map[answerKey[right]]+=1
-            else:
-                map[answerKey[right]]=1
+            map[answerKey[right]] = map.get(answerKey[right],0)+1
             maxfreq = max(maxfreq,map[answerKey[right]])
-            if (right-left+1) - maxfreq > k:
+            while (right-left+1) - maxfreq > k:
                 map[answerKey[left]]-=1
                 left+=1
             maxlen = max(maxlen,right-left+1)
