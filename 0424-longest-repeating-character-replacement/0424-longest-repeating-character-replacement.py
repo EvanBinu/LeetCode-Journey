e18346515ea74c42ba3f5010ddb596e5efcecdfa
Evan Binu Chirayath <143860161+EvanBinu@users.noreply.github.com>
2026-07-31 18:33:59 +0530

    Time: 85 ms (52.23%), Space: 19.6 MB (50.39%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
@@ -8,7 +8,7 @@ class Solution:
         for right in range(n):
             map[s[right]] = map.get(s[right],0)+1
             maxfreq = max(maxfreq,map[s[right]])
-            while (right-left+1) - maxfreq > k:
+            if (right-left+1) - maxfreq > k:
                 if s[left] in map:
                     map[s[left]]-=1
                 if map[s[left]]==0:
