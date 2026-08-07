c5c6ce1ef0b9156733a8e7734037adc2a2e30b00
Evan Binu Chirayath <143860161+EvanBinu@users.noreply.github.com>
2026-08-07 19:19:25 +0530

    Time: 19 ms (60.54%), Space: 19.4 MB (18.25%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.py
+++ 0567-permutation-in-string/0567-permutation-in-string.py
@@ -2,16 +2,16 @@ class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
         k = len(s1)
         n = len(s2)
-        freq1 = [0]*26
-        freq2 = [0]*26
+        map1 = [0]*26
+        map2 = [0]*26
         left = 0
         for i in range(k):
-            freq1[ord('a')-ord(s1[i])]+=1
-        for right in range(n):
-            freq2[ord('a')-ord(s2[right])]+=1
-            if (right - left + 1) >= k:
-                if freq1==freq2:
+            map1[ord('a') - ord(s1[i])]+=1
+        for j in range(n):
+            map2[ord('a') - ord(s2[j])]+=1
+            if (j - left + 1) ==k:
+                if map1 == map2:
                     return True
-                freq2[ord('a')-ord(s2[left])]-=1
+                map2[ord('a') - ord(s2[left])]-=1
                 left+=1
-        return False
+        return False 
