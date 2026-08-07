e1fd494fce3a1321cd85b8039c1c2ee33ec64176
Evan Binu Chirayath <143860161+EvanBinu@users.noreply.github.com>
2026-08-07 19:57:04 +0530

    Time: 178 ms (76.58%), Space: 26 MB (13.38%) - LeetHub

--- 0904-fruit-into-baskets/0904-fruit-into-baskets.py
+++ 0904-fruit-into-baskets/0904-fruit-into-baskets.py
@@ -2,17 +2,13 @@ class Solution:
     def totalFruit(self, fruits: List[int]) -> int:
         map = {}
         left = 0
-        ms = 0
+        mlen = 0
         for right in range(len(fruits)):
-            if fruits[right] in map:
-                map[fruits[right]]+=1
-            else:
-                map[fruits[right]] = 1
-            if len(map) > 2:
-                if fruits[left] in map:
-                    map[fruits[left]]-=1
-                if map[fruits[left]] == 0:
+            map[fruits[right]] = map.get(fruits[right],0)+1
+            while len(map) > 2:
+                map[fruits[left]]-=1
+                if map[fruits[left]] <=0:
                     del map[fruits[left]]
                 left+=1
-            ms = max(ms,right-left+1)
-        return ms
+            mlen = max(right-left+1,mlen)
+        return mlen
